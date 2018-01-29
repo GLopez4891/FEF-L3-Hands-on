0ad714c48fbb7f2cf75378ca02b2584923629d63
Glenn Lopez <glenn.lopez2015@yahoo.com>
2018-01-28 20:47:10 -0500

    added files

---
+++ script.js
@@ -0,0 +1,20 @@
+let highSchoolGrade = 11;
+let yourGrade;
+
+switch(highSchoolGrade){
+case 9:
+yourGrade = "Freshman";
+break;
+case 10:
+yourGrade = "Sophmore";
+break;
+case 11:
+yourGrade = "Junior";
+break;
+case 12:
+yourGrade = "Senior"
+default:
+yourGrade = "invalid";
+}
+console.log(yourGrade);
+
